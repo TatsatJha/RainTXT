2fdd91b169c70f81046d0603747cafa2caf4c12b
Tatsat Jha <TatsatTarun@gmail.com>
2022-03-11 19:19:05 -0700

    Create starting components

---
+++ Components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className='text-5xl p-6'>RainTXT</h1>
+    </div>
+  )
+}
---
+++ pages/edit.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextEditor from '../Components/Editor/TextEditor'
+
+function edit() {
+  return (
+    <div>
+      <TextEditor></TextEditor>
+    </div>
+  )
+}
+
+export default edit
