347574338a7d42ddefa6c600162ae96881b4076b
Tatsat Jha <TatsatTarun@gmail.com>
2022-03-13 09:51:00 -0700

    allow saved text to be shown in docs

--- pages/edit/[id].tsx
+++ pages/edit/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TextEditor from '../../Components/Edit/TextEditor'
 import Edit from '../../Components/Edit/Edit'
 import { useRouter } from 'next/router'
@@ -8,6 +8,17 @@ export default function editDoc() {
     const router = useRouter()
     const { id } = router.query
     const [text, setText] = useState("")
+
+
+    useEffect(()=> {
+      const getDocs = async () => {
+          await axios.get(`http://localhost:3000/api/doc/${id}`).then((response) => {
+              const existingText = response.data.content
+              console.log(existingText)
+              setText(existingText)
+          })
+      }
+  getDocs()}, [])
 
     const handleSave = ()=>{
       axios.put(`http://localhost:3000/api/doc/${id}`, {content:text})
