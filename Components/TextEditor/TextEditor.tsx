aabe7ceecd824a7a4a64c2b75c151fe7c79313bf
Tatsat Jha <TatsatTarun@gmail.com>
2022-03-26 11:57:14 -0700

    style an improved textarea

--- Components/TextEditor/TextEditor.tsx
+++ Components/TextEditor/TextEditor.tsx
@@ -4,8 +4,14 @@ import SideBar from './SideBar'
 export default function TextEditor(props:any) {
 
     return (
-        <div className="">
-            <input value= {props.text} onChange={(e)=>props.setText(e.target.value)} className='border-2 w-fit p-64' type="text"></input>
+        <div className="flex justify-center">
+            <textarea style={
+                {
+                    border: "none",
+                    outline: "none",
+                    resize: "none"
+                }
+            } value= {props.text} onChange={(e)=>props.setText(e.target.value)} className='border-2 px-[1in] py-[1in] w-[8.5in] h-[11in] shadow-xl mt-32'></textarea>
         </div>
     )
 }
--- pages/edit/[id].tsx
+++ pages/edit/[id].tsx
@@ -26,7 +26,7 @@ export default function editDoc({ data }: any) {
       axios.put(`http://localhost:3000/api/doc/${id}`, {content:text})
     }
   return (
-    <div className=''>
+    <div className='bg-slate-50'>
         <Edit setText= {setText} text= {text}></Edit>
         <button onClick={handleSave}>Save</button>
     </div>
