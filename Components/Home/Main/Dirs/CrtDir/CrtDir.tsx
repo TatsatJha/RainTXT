e4ac39c7088b9af024a76700667d6ded73d56565
Tatsat Jha <TatsatTarun@gmail.com>
2022-04-02 14:55:27 -0700

    add menu for directory create button

--- Components/Home/Main/Dirs/CrtDir/CrtDir.tsx
+++ Components/Home/Main/Dirs/CrtDir/CrtDir.tsx
@@ -1,15 +1,36 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import {BsFolderPlus} from "react-icons/bs"
 
 export default function CrtDir() {
+const [open, setOpen] = useState(false)
+const [value, setValue] = useState("Untitled Folder")
     const  makeDir = ()=>{
-        axios.post("http://localhost:3000/api/dir", {title: "djlfkdjs", docs: ""})
+        axios.post("http://localhost:3000/api/dir", {title: value, docs: ""})
+        setOpen(!open)
+    }
+
+    const abort = ()=>{
+      setValue("Untitled Folder")
+      setOpen(!open)
+    }
+
+    if(open){
+      return(
+        <div className='fixed p-8 left-[800px] bg-slate-200 border-4'>
+          <p className='p-4'>Name of Workspace</p>
+          <input className='' type="text" value={value} onChange={(e)=>{setValue(e.target.value)}}></input>
+          <div className='p-4 flex justify-evenly'>
+            <button onClick={abort}>Cancel</button>
+            <button onClick={makeDir} >Create</button>
+          </div>
+        </div>
+      )
     }
   return (
     <div className='flex p-6'>
         <BsFolderPlus size="28px"></BsFolderPlus>
-        <button className= "text-l px-4" onClick={makeDir}> New Workspace </button>
+        <button className= "text-l px-4" onClick={()=>{setOpen(true)}}> New Workspace </button>
     </div>
   )
 }
--- Components/Home/Main/Main.tsx
+++ Components/Home/Main/Main.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 import Docs from "./Docs"
 import Dirs from "./Dirs"
 
 export default function Main(props:any) {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <div>
-      <Docs refresh={props.refresh} data={props.data}></Docs>
-      <Dirs></Dirs>
-      <NavBar refresh={props.refresh}></NavBar>
+      <main>
+        <Docs refresh={props.refresh} data={props.data}></Docs>
+        <Dirs></Dirs>
+        <NavBar refresh={props.refresh}></NavBar>
+      </main>
     </div>
   )
 }
