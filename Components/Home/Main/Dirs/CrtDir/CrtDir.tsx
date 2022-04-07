3f54beeb3e44c87231d72b5c81f13b007ecaf1d3
TatsatJha <TatsatTarun@gmail.com>
2022-04-07 14:00:20 -0600

    adjust styling

--- Components/Home/Main/Dirs/CrtDir/CrtDir.tsx
+++ Components/Home/Main/Dirs/CrtDir/CrtDir.tsx
@@ -19,9 +19,9 @@ const [value, setValue] = useState("Untitled Folder")
 
     if(open){
       return(
-        <div className='fixed p-8 left-[800px] bg-slate-200 border-4'>
-          <p className='p-4'>Name of Workspace</p>
-          <input className='' type="text" value={value} onChange={(e)=>{setValue(e.target.value)}}></input>
+        <div className='fixed p-8 left-[800px] bg-white border-4'>
+          <p className='p-4 text-xl text-center'>Name of Workspace</p>
+          <input className='bg-slate-100 rounded-xl p-4' type="text" value={value} onChange={(e)=>{setValue(e.target.value)}}></input>
           <div className='p-4 flex justify-evenly'>
             <button onClick={abort}>Cancel</button>
             <button onClick={makeDir} >Create</button>
